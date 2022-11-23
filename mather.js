8f599d40d9ead8e1d5bb46afd6e822699373dd86
asama1 <79220179+asama1@users.noreply.github.com>
2022-11-23 09:30:25 -0500

    Update mather.js
    not in use anymore

--- mather.js
+++ mather.js
@@ -1,19 +1,19 @@
-class mather {
-  constructor(){
-    this.xpos = 40;
-    this.ypos = 40;
-    this.fatness = 45;
-    this.shortness = 85;
-  }
+//class mather {
+ // constructor(){
+ //   this.xpos = 40;
+ //   this.ypos = 40;
+ //   this.fatness = 45;
+  //  this.shortness = 85;
+ // }
   
   
-  show(){
-   rect(this.xpos, this.ypos, this.fatness, this.shortness) 
-  }
+ // show(){
+ //  rect(this.xpos, this.ypos, this.fatness, this.shortness) 
+//  }
   
-  update(){
+//  update(){
     
-  }
+//  }
   
   
-}
+//}
